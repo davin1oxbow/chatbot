a17b3dec6f4afaa2d2c48bed02b10ce9e58bd8b1
Davin Jeong <husky.davin@gmail.com>
2024-07-16 23:06:38 -0700

    original rispose script

--- script.js
+++ script.js
@@ -193,6 +193,5 @@ async function startChatBox() {
   for (const msg of history) addMessage(msg.content, msg.role, !0);
   history.length || addMessage(welcome, "assistant", !0);
   
-  togglePopup();
 }
 startChatBox();
