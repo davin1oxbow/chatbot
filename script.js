f7f6893d917f0c573d11697cf532ac4c11d1c321
Davin Jeong <husky.davin@gmail.com>
2024-07-16 23:01:49 -0700

    Use original script from rispose

--- script.js
+++ script.js
@@ -192,7 +192,5 @@ async function startChatBox() {
     history = fetchHistory();
   for (const msg of history) addMessage(msg.content, msg.role, !0);
   history.length || addMessage(welcome, "assistant", !0);
-
-  togglePopup();
 }
 startChatBox();
